5526d98b9635e5901365b302b01f005c69d847b4
Sung Ji Hoon <sungjihoon@gmail.com>
2016-01-19 00:34:23 +0900

    add - chapter01/exercise/e01

---
+++ springstudy/src/main/java/chapter01/exercise/ex01/UserDao.java
@@ -0,0 +1,45 @@
+package chapter01.exercise.ex01;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDao {
+
+	public void add(User user) throws ClassNotFoundException, SQLException {
+		Class.forName("org.postgresql.Driver");
+		Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost/postgres", "postgres", "bbada");
+		PreparedStatement statement = connection.prepareStatement("insert into USERS (ID, NAME, PASSWORD) values (?, ?, ?)");
+		statement.setString(1, user.getId());
+		statement.setString(2, user.getName());
+		statement.setString(3, user.getPassword());
+		
+		statement.executeUpdate();
+		
+		statement.close();
+		connection.close();
+	}
+	
+	public User get(String id) throws ClassNotFoundException, SQLException {
+		Class.forName("org.postgresql.Driver");
+		Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost/postgres", "postgres", "bbada");
+		PreparedStatement statement = connection.prepareStatement("select * from USERS where ID = ?");
+		statement.setString(1, id);
+		
+		ResultSet resultSet = statement.executeQuery();
+		resultSet.next();
+		
+		User user = new User();
+		user.setId(resultSet.getString("ID"));
+		user.setName(resultSet.getString("NAME"));
+		user.setPassword(resultSet.getString("PASSWORD"));
+
+		resultSet.close();
+		statement.close();
+		connection.close();
+		
+		return user;
+	}
+}
---
+++ springstudy/src/main/java/chapter01/exercise/ex01/UserDaoTest.java
@@ -0,0 +1,24 @@
+package chapter01.exercise.ex01;
+
+import java.sql.SQLException;
+
+public class UserDaoTest {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		UserDao userDao = new UserDao();
+		
+		User user = new User();
+		user.setId("Yuna");
+		user.setName("김연아");
+		user.setPassword("승냥이");
+		
+		userDao.add(user);
+		System.out.println(user.getId() + " 등록 성공");
+		
+		User user2 = userDao.get(user.getId());
+		System.out.println(user2.getName());
+		System.out.println(user2.getPassword());
+		
+		System.out.println(user2.getId() + " 조회 성공");
+	}
+}
