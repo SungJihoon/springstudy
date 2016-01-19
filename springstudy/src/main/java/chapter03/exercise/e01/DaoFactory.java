127fb108ce3aedb3af1bf803eced0b23a302d861
Sung Ji Hoon <sungjihoon@gmail.com>
2016-01-20 01:06:34 +0900

    add - chapter03/exercise/e01

---
+++ springstudy/src/main/java/chapter03/exercise/e01/DaoFactory.java
@@ -0,0 +1,24 @@
+package chapter03.exercise.e01;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+@Configuration
+public class DaoFactory {
+	
+	@Bean
+	public UserDao userDao() {
+		UserDao userDao = new UserDao();
+		userDao.setDataSource(dataSource());
+		return userDao;
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		return new SimpleDriverDataSource();
+	}
+
+}
