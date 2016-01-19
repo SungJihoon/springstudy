f05c8ebc76955de5630d89d6216690a739f2a202
Sung Ji Hoon <sungjihoon@gmail.com>
2016-01-19 22:33:24 +0900

    add - chapter01/exercise/e10

---
+++ springstudy/src/main/java/chapter01/exercise/ex10/BConnectionMaker.java
@@ -0,0 +1,15 @@
+package chapter01.exercise.ex10;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BConnectionMaker implements ConnectionMaker {
+
+	public Connection makeConnection() throws ClassNotFoundException, SQLException  {
+		Class.forName("org.postgresql.Driver");
+		Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost/postgres", "postgres", "bbada");
+		return connection;
+	}
+
+}
