b3ca4b86a7003b299ddf72e9db7b6cce70ead0ef
Sung Ji Hoon <sungjihoon@gmail.com>
2016-01-19 22:35:55 +0900

    add - chapter01/practice

---
+++ springstudy/src/main/java/chapter01/practice/UserDaoTest.java
@@ -0,0 +1,29 @@
+package chapter01.practice;
+
+import java.sql.SQLException;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class UserDaoTest {
+
+	public static void main(String[] args) throws SQLException {
+		
+		ApplicationContext context = new GenericXmlApplicationContext("chapter01/practice/daoContext.xml");
+		UserDao userDao = context.getBean("userDao", UserDao.class);
+		
+		User user = new User();
+		user.setId("Yuna");
+		user.setName("김연아");
+		user.setPassword("승냥이");
+		
+		userDao.add(user);
+		System.out.println(user.getId() + " 등록 성공");
+		
+		User user2 = userDao.get(user.getId());
+		System.out.println(user2.getName());
+		System.out.println(user2.getPassword());
+		
+		System.out.println(user2.getId() + " 조회 성공");
+	}
+}
