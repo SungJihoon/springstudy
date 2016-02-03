7aba155a2f37b8c726a05355a4df755db1a186f9
Sung Ji Hoon <sungjihoon@gmail.com>
2016-02-03 23:05:16 +0900

    UserDao 예외처리
    try ~ catch ~ finally 적용

--- springstudy/src/main/java/chapter03/exercise/e01/UserDao.java
+++ springstudy/src/main/java/chapter03/exercise/e01/UserDao.java
@@ -17,63 +17,159 @@ public class UserDao {
 		this.dataSource = dataSource;
 	}
 
+	public void deleteAll() throws SQLException {
+		Connection connection = null;
+		PreparedStatement statement = null;
+		
+		try {
+			connection = dataSource.getConnection();
+			statement = connection.prepareStatement("delete from USERS");
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			throw e;
+		} finally {
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+			
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+		}
+	}
+
 	public void add(User user) throws SQLException {
-		Connection connection = dataSource.getConnection();
-		PreparedStatement statement = connection.prepareStatement("insert into USERS (ID, NAME, PASSWORD) values (?, ?, ?)");
-		statement.setString(1, user.getId());
-		statement.setString(2, user.getName());
-		statement.setString(3, user.getPassword());
+		Connection connection = null;
+		PreparedStatement statement = null;
 		
-		statement.executeUpdate();
-		
-		statement.close();
-		connection.close();
+		try {
+			connection = dataSource.getConnection();
+			statement = connection.prepareStatement("insert into USERS (ID, NAME, PASSWORD) values (?, ?, ?)");
+			statement.setString(1, user.getId());
+			statement.setString(2, user.getName());
+			statement.setString(3, user.getPassword());
+			
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			throw e;
+		} finally {
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+			
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+		}
 	}
 	
 	public User get(String id) throws SQLException {
-		Connection connection = dataSource.getConnection();
-		PreparedStatement statement = connection.prepareStatement("select * from USERS where ID = ?");
-		statement.setString(1, id);
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
 		
-		ResultSet resultSet = statement.executeQuery();
-		User user = null;
-		if (resultSet.next()) {
-			user = new User();
-			user.setId(resultSet.getString("ID"));
-			user.setName(resultSet.getString("NAME"));
-			user.setPassword(resultSet.getString("PASSWORD"));
+		try {
+			connection = dataSource.getConnection();
+			statement = connection.prepareStatement("select * from USERS where ID = ?");
+			statement.setString(1, id);
+			
+			resultSet = statement.executeQuery();
+			User user = null;
+			if (resultSet.next()) {
+				user = new User();
+				user.setId(resultSet.getString("ID"));
+				user.setName(resultSet.getString("NAME"));
+				user.setPassword(resultSet.getString("PASSWORD"));
+			}
+			if (user == null) throw new EmptyResultDataAccessException(1);	
+			return user;
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			throw e;
+		} finally {
+			if (resultSet != null) {
+				try {
+					resultSet.close();
+				} catch (SQLException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+			
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+			
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					System.out.println(e.getMessage());
+				}
+			}
 		}
-
-		resultSet.close();
-		statement.close();
-		connection.close();
-
-		if (user == null) throw new EmptyResultDataAccessException(1);
-		
-		return user;
 	}
 	
-	public void deleteAll() throws SQLException {
-		Connection connection = dataSource.getConnection();
-		PreparedStatement statement = connection.prepareStatement("delete from USERS");
-		statement.executeUpdate();
-		
-		statement.close();
-		connection.close();
-	}
-
 	public int getCount() throws SQLException {
-		Connection connection = dataSource.getConnection();
-		PreparedStatement statement = connection.prepareStatement("select count(*) from USERS");
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
 		
-		ResultSet resultSet = statement.executeQuery();
-		resultSet.next();
-		int count = resultSet.getInt(1);
-		
-		resultSet.close();
-		statement.close();
-		connection.close();
-		
-		return count;
+		try {
+			connection = dataSource.getConnection();
+			statement = connection.prepareStatement("select count(*) from USERS");
+			
+			resultSet = statement.executeQuery();
+			resultSet.next();
+			return resultSet.getInt(1);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			throw e;
+		} finally {
+			if (resultSet != null) {
+				try {
+					resultSet.close();
+				} catch (SQLException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+			
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+			
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+		}
 	}
 }
