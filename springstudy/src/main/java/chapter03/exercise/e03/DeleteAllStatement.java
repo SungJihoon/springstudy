3e779f9865c8129241972bae8f085454b0b7a5f2
Sung Ji Hoon <sungjihoon@gmail.com>
2016-02-06 21:33:59 +0900

    Add Interface StatementStrategy
    PreparedStatement 를 반환해 주는 StatementStrategy Interface 생성.
    deleteAll() 메소드 기능을 구현한 StatementStrategy 전략 클래스인 DeleteAllStatement 생성.
    deleteAll() 메소드에 전략패턴이 적용된 DeleteAllStatement 를 적용.

---
+++ springstudy/src/main/java/chapter03/exercise/e03/DeleteAllStatement.java
@@ -0,0 +1,13 @@
+package chapter03.exercise.e03;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DeleteAllStatement implements StatementStrategy {
+
+	public PreparedStatement makePreparedStatement(Connection connection) throws SQLException {
+		return connection.prepareStatement("delete from USERS");
+	}
+
+}
