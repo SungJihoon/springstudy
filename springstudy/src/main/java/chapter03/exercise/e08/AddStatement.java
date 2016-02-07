77856f3093a5c9cd22f5829c8d466f81990aa387
Sung Ji Hoon <sungjihoon@gmail.com>
2016-02-07 21:34:41 +0900

    AddStatement 를 익명 내부 클래스로 전환

---
+++ springstudy/src/main/java/chapter03/exercise/e08/AddStatement.java
@@ -0,0 +1,23 @@
+package chapter03.exercise.e08;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class AddStatement implements StatementStrategy {
+
+	User user;
+	
+	public AddStatement(User user) {
+		this.user = user;
+	}
+	
+	public PreparedStatement makePreparedStatement(Connection connection) throws SQLException {
+		PreparedStatement statement = connection.prepareStatement("insert into USERS (ID, NAME, PASSWORD) values (?, ?, ?)");
+		statement.setString(1, user.getId());
+		statement.setString(2, user.getName());
+		statement.setString(3, user.getPassword());
+		return statement;
+	}
+
+}
