22dbe8d247b9c0d33952cd7292cefad0e9536da9
Sung Ji Hoon <sungjihoon@gmail.com>
2016-02-08 18:03:28 +0900

    콜백의 분리와 재활용
    deleteAll() 의 jdbcContext 를 Extract Method 리팩토링 기법을 이용해 추출.
    executeSql() 을 만들어서 재활용 가능한 콜백을 담는 메소드를 생성.

---
+++ springstudy/src/main/java/chapter03/exercise/e12/UserDao.java
@@ -0,0 +1,139 @@
+package chapter03.exercise.e12;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+
+public class UserDao {
+	
+	DataSource dataSource;
+	private JdbcContext jdbcContext;
+	
+	public void setDataSource(DataSource dataSource) {
+		this.jdbcContext = new JdbcContext();
+		this.jdbcContext.setDataSource(dataSource);
+		this.dataSource = dataSource;
+	}
+
+	public void deleteAll() throws SQLException {
+		executeSql("delete from USERS");
+	}
+
+	private void executeSql(final String query) throws SQLException {
+		this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
+			public PreparedStatement makePreparedStatement(Connection connection) throws SQLException {
+				return connection.prepareStatement(query);
+			}
+		});
+	}
+
+	public void add(final User user) throws SQLException {
+		this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
+			public PreparedStatement makePreparedStatement(Connection connection) throws SQLException {
+				PreparedStatement statement = connection
+						.prepareStatement("insert into USERS (ID, NAME, PASSWORD) values (?, ?, ?)");
+				statement.setString(1, user.getId());
+				statement.setString(2, user.getName());
+				statement.setString(3, user.getPassword());
+				return statement;
+			}
+		});
+	}
+
+	public User get(String id) throws SQLException {
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		
+		try {
+			connection = dataSource.getConnection();
+			statement = connection.prepareStatement("select * from USERS where ID = ?");
+			statement.setString(1, id);
+			
+			resultSet = statement.executeQuery();
+			User user = null;
+			if (resultSet.next()) {
+				user = new User();
+				user.setId(resultSet.getString("ID"));
+				user.setName(resultSet.getString("NAME"));
+				user.setPassword(resultSet.getString("PASSWORD"));
+			}
+			if (user == null) throw new EmptyResultDataAccessException(1);	
+			return user;
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			throw e;
+		} finally {
+			if (resultSet != null) {
+				try {
+					resultSet.close();
+				} catch (SQLException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+			
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+			
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+		}
+	}
+	
+	public int getCount() throws SQLException {
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		
+		try {
+			connection = dataSource.getConnection();
+			statement = connection.prepareStatement("select count(*) from USERS");
+			
+			resultSet = statement.executeQuery();
+			resultSet.next();
+			return resultSet.getInt(1);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			throw e;
+		} finally {
+			if (resultSet != null) {
+				try {
+					resultSet.close();
+				} catch (SQLException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+			
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+			
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+		}
+	}
+}
