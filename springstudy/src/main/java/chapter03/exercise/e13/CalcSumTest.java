473507f4d14b5192b77a6c25191e2141092ead4a
Sung Ji Hoon <sungjihoon@gmail.com>
2016-02-08 18:43:39 +0900

    템플릿/콜백의 응용
    파일의 숫자 합을 계산하는 코드의 테스트

---
+++ springstudy/src/main/java/chapter03/exercise/e13/CalcSumTest.java
@@ -0,0 +1,19 @@
+package chapter03.exercise.e13;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class CalcSumTest {
+	
+	@Test
+	public void sumOfNumbers() throws IOException {
+		Calculator calculator = new Calculator();
+		int sum = calculator.calcSum(getClass().getResource("numbers.txt").getPath());
+		assertThat(sum, is(10));
+	}
+
+}
